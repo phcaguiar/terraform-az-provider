2c5d30e4d3644b278f739f87ecfd2dade05bc875
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-06 03:42:17 +0000

    module refactoring

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "azurerm" {
-   subscription_id   =  "${var.subscription}"
+   subscription_id               =  "${var.subscription_id}"
+   client_id                     =  "${var.client_id}"
+   client_secret                 =  "${var.client_secret}"
+   tenant_id                     =  "${var.tenant_id}"
+   use_msi                       =  "${var.use_msi}"
+   msi_endpoint                  =  "${var.msi_endpoint}"
+   environment                   =  "${var.environment}"
+   skip_credentials_validation   =  "${var.skip_credentials_validation}"
+   skip_provider_registration    =  "${var.skip_provider_registration}"
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,44 @@
-variable "subscription" {}
+variable "subscription_id" {
+  description = "Default subscription to create you environment."
+  default     = ""
+}
+
+variable "client_id" {
+  description = "Default prefix to use with your resource names."
+  default     = ""
+}
+
+variable "client_secret" {
+  description = "Default prefix to use with your resource names."
+  default     = ""
+}
+
+variable "tenant_id" {
+  description = "Default prefix to use with your resource names."
+  default     = ""
+}
+
+variable "use_msi" {
+  description = "Default prefix to use with your resource names."
+  default     = ""
+}
+
+variable "msi_endpoint" {
+  description = "Default prefix to use with your resource names."
+  default     = ""
+}
+
+variable "environment" {
+  description = "Default prefix to use with your resource names."
+  default     = "public"
+}
+
+variable "skip_credentials_validation" {
+  description = "Default prefix to use with your resource names."
+  default     = "false"
+}
+
+variable "skip_provider_registration" {
+  description = "Default prefix to use with your resource names."
+  default     = "false"
+}
